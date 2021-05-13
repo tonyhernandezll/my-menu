177f94c8c10790eeb859730015828af6cc02061b
Tony <tonyhll1972@gmail.com>
2021-05-12 19:43:26 -0500

    added filtering functionality to the buttons

--- setup/app.js
+++ setup/app.js
@@ -85,6 +85,17 @@ window.addEventListener("DOMContentLoaded", function () {
 filterBtns.forEach(function(btn) {
   btn.addEventListener("click", function (e) {
     const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter(function(menuItem) {
+      if (menuItem.category === category) {
+        return menuItem;
+
+      }
+    }); 
+    if (category === 'all') {
+      displayMenuItems(menu);
+    } else {
+      displayMenuItems(menuCategory);
+    }
   });
 });
 
