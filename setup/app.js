c3b665b66882b33a990252cf695a31d188a30924
Tony <tonyhll1972@gmail.com>
2021-05-12 19:18:31 -0500

    added 4 buttons, one for all, breakfast, lunch, and shakes

--- setup/app.js
+++ setup/app.js
@@ -75,8 +75,21 @@ const menu = [
 
 const sectionCenter = document.querySelector('.section-center');
 
+const filterBtns = document.querySelectorAll('.filter-btn');
+
+// load items
 window.addEventListener("DOMContentLoaded", function () {
-  let displayMenu = menu.map(function(item) {
+  displayMenuItems(menu);
+});
+// filters items
+filterBtns.forEach(function(btn) {
+  btn.addEventListener("click", function (e) {
+    const category = e.currentTarget.dataset.id;
+  });
+});
+
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function(item) {
     return `<article class="menu-item">
     <img src=${item.img} class="photo" alt=${item.title} />
     <div class="item-info">
@@ -94,4 +107,4 @@ window.addEventListener("DOMContentLoaded", function () {
   });
   displayMenu = displayMenu.join("");
   sectionCenter.innerHTML = displayMenu;
-});
+};
